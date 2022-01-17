8c9dfe7a68a756dbe336628c79d4a0f2a622ba15
Aleksandra <astarta1107@gmail.com>
2022-01-17 19:24:25 +0300

    deploy

---
+++ precache-manifest.37ab2be11a5d6500ee8cee958fc0b45f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c1d19ae74dd1d2bf5f",
+    "url": "/vue-crm-app/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "1d69a46276f2a8e3d710",
+    "url": "/vue-crm-app/css/chunk-2121d9d5.e3e6b04a.css"
+  },
+  {
+    "revision": "51c43e10add0d3d41d4b",
+    "url": "/vue-crm-app/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "401763f582e47d9f515389af957a908b",
+    "url": "/vue-crm-app/index.html"
+  },
+  {
+    "revision": "32c1d19ae74dd1d2bf5f",
+    "url": "/vue-crm-app/js/app.f87286af.js"
+  },
+  {
+    "revision": "ec3ef5e5d768d16dd28a",
+    "url": "/vue-crm-app/js/chunk-0fe47070.5bc2c745.js"
+  },
+  {
+    "revision": "532a0906c0ff6313bd05",
+    "url": "/vue-crm-app/js/chunk-10b86dd9.12490b7c.js"
+  },
+  {
+    "revision": "1d69a46276f2a8e3d710",
+    "url": "/vue-crm-app/js/chunk-2121d9d5.4f59f0ce.js"
+  },
+  {
+    "revision": "eb3e5b33afe8559dbd97",
+    "url": "/vue-crm-app/js/chunk-2d0d7699.1f5a5268.js"
+  },
+  {
+    "revision": "dbba5f87f98723e9a597",
+    "url": "/vue-crm-app/js/chunk-2d21a3d2.edaf9e4e.js"
+  },
+  {
+    "revision": "ed5ac364194336652928",
+    "url": "/vue-crm-app/js/chunk-53c8ec65.3d1906b6.js"
+  },
+  {
+    "revision": "ba8b8dbd75554cc9fcea",
+    "url": "/vue-crm-app/js/chunk-5f4ad635.b1e268dc.js"
+  },
+  {
+    "revision": "a0f3fb2f58974d666694",
+    "url": "/vue-crm-app/js/chunk-927bacd6.f24209d0.js"
+  },
+  {
+    "revision": "94ae85ee06fdf7d9baf0",
+    "url": "/vue-crm-app/js/chunk-c420df12.24f98fec.js"
+  },
+  {
+    "revision": "51c43e10add0d3d41d4b",
+    "url": "/vue-crm-app/js/chunk-vendors.f24fe274.js"
+  },
+  {
+    "revision": "ebb49558d541548e106255507993b277",
+    "url": "/vue-crm-app/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-crm-app/robots.txt"
+  }
+]);
